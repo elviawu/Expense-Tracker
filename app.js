e3b1bcd0da1dd251b942522e059ff63c766cf447
elviawu <elviawu@gmail.com>
2023-05-02 17:36:20 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
+const mongoose = require('mongoose') // 載入 mongoose
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const app = express()
 const port = 3000
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 
 // 設定首頁路由
 app.get('/', (req, res) => {
