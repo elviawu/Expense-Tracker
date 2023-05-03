f7a02914c1a0ec1f2cbe4fe528e5a62888507ee7
elviawu <elviawu@gmail.com>
2023-05-03 20:13:58 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
+const exphbs = require('express-handlebars');
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -10,9 +11,12 @@ const port = 3000
 
 require('./config/mongoose')
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 // 設定首頁路由
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.render('index')
 })
 
 // 設定 port 3000
