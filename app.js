9b3ce590ffe73191515eed19914dbbe635fd3f80
elviawu <elviawu@gmail.com>
2023-05-04 15:15:42 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const session = require('express-session')
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser')
+const flash = require('connect-flash')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -25,9 +26,12 @@ app.use(session({
 }))
 app.use(bodyParser.urlencoded({ extended: true }))
 usePassport(app)
+app.use(flash())
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
   next()
 })
 app.use(routes)
